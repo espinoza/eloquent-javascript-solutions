6874cbdd37d3a522e283f9d04bf6843e47b321cc
Jorge Espinoza <jorgeespinozaortega@gmail.com>
2020-10-31 20:49:14 -0300

    Add recursion.js

---
+++ recursion.js
@@ -0,0 +1,14 @@
+// A program that contains a function to check if a number
+// is even or odd recursively
+
+function isEven(number) {
+	if (number < 0) number = -number;
+	if (number == 0) return true;
+	if (number == 1) return false;
+	if (number >= 2) return isEven(number - 2);
+}
+
+console.log("Testing function isEven:");
+console.log("Is 50 even?", isEven(50));
+console.log("Is 75 even?", isEven(75));
+console.log("Is -1 even?", isEven(-1));

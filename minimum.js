bf85132e5ccdc145c4a92e7d2884f73f8dd3aca2
Jorge Espinoza <jorgeespinozaortega@gmail.com>
2020-10-31 20:38:33 -0300

    Add minimum.js

---
+++ minimum.js
@@ -0,0 +1,15 @@
+// A program that contains a function to obtain the minimum
+// between two numbers
+
+function min(a, b) {
+	if (a - b < 0) {
+		return a;
+	} else {
+		return b;
+	}
+}
+
+console.log("Testing function min:")
+console.log("min(4, 5) =", min(4, 5))
+console.log("min(-2, -7) =", min(-2, -7))
+console.log("min(10, 10) =", min(10, 10))
